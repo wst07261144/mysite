b289107b7406f7f27effe8920c712f8c10ff226f
wst07261144 <wst07261144@gmail.com>
2019-03-04 16:52:11 +0800

    Question 对象被管理

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):  # 给模型增加 __str__() 方法是很重要的，这不仅仅能给你在命令行里使用带来方便，Django 自动生成的 admin 里也使用这个方法来表示对象。
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
